c4417e0248db36d9177836b578528550d9091731
Deputy Mashahary <detech1960@gmail.com>
2024-07-14 12:42:49 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -17,7 +17,7 @@ from src.utils import save_object
 import os
 
 class DataTransformationConfig:
-    preprocessor_ob_file_path = os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
     
 class DataTransformation:
     def __init__(self):
@@ -83,7 +83,7 @@ class DataTransformation:
             target_feature_train_df = train_df[target_column_name]
             
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
-            target_feaature_test_df = test_df[target_column_name]
+            target_feature_test_df = test_df[target_column_name]
             
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
@@ -94,18 +94,18 @@ class DataTransformation:
             
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feaature_test_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
             logging.info("Saved processing object.")
             
             save_object(
-                file_path = self.data_transformation_config.preprocessor_ob_file_path,
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
             )
             
             return(
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_ob_file_path,
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
             
                
